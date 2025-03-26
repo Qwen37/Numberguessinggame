b45ac38fd7c50edd443e23bff267f8ab7d69a978
lab_services_student <qwensewdeen1705@gmail.com>
2025-03-26 15:23:32 +0200

    2nd commit

---
+++ app/src/main/java/com/qwen/numberguessinggame/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.qwen.numberguessinggame
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }  //end of ViewCompat
+
+//code starts here
+
+//connect the backend to the frontend elements
+val userGuess = findViewById<EditText>(R.id.edtUserGuess)
+val tvAnswer = findViewById<TextView>(R.id.tvAnswer)
+val guessButton = findViewById<Button>(R.id.btnGuess)
+var count: Int = 0
+
+//creates the button listener or click action
+guessButton.setOnClickListener {
+    // create a random number between 1 and 10
+    val randomNumber = Random.nextInt(1, 11)
+
+    //get the user guess from the element and saves it into a variable
+    var guessNumberString = userGuess.text.toString()
+    //creates an empty integer variable
+    var guessNumberInt: Int
+    when {
+        //checks if the guess is a number
+        guessNumberString.toIntOrNull() != null -> {
+            //if yes, converts the guess to an integer
+            guessNumberInt = guessNumberString.toInt()
+            //check guess against random number
+            if (guessNumberInt == randomNumber && count < 5) {
+                //if yes, displays a toast message
+                Toast.makeText(this, "You guessed correctly", Toast.LENGTH_SHORT)
+            } else if (count >= 5){
+                Toast.makeText(this, "You have reached the maximum number of guesses", Toast.LENGTH_SHORT)
+            } else {
+                //if no, displays a toast message
+                Toast.makeText(this, "You guessed incorrectly", Toast.LENGTH_SHORT).show()
+                //increments the count by 1
+                count++
+            }
+            //sets the tvAnswer to the random number if the guess was a number
+            tvAnswer.text = randomNumber.toString()
+        }
+        //if guess was not a number, displays a toast message
+        else -> {
+            Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
+        } //end of else
+    } //end of when
+} //end of guessButton.setOnClickListener
+} //end of onCreate
+} //end
